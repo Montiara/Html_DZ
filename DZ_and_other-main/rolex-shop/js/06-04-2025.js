f3f6a98eeabe9473b1e117b78aee4c0cc5de45c6
MontЯra <Ivanmonty15@gmail.com>
2025-04-13 15:30:48 +0300

    sasiska

--- DZ_and_other-main/rolex-shop/js/06-04-2025.js
+++ DZ_and_other-main/rolex-shop/js/06-04-2025.js
@@ -1,31 +1,45 @@
 // +
 
-let num1, num2, sign, result = NaN;
-
-num1 = Number(prompt('введите первое число:','0'));
-num2 = Number(prompt('введите первое число:','0'));
-sign = prompt('введите действие(+,-,*,/):','+');
-
-switch(sign){
-    case "":
-        result = num1 + num2;
-        break;
-    case "":
-        result = num1 - num2;
-        break;
-    case "":
-        result = num1 * num2;
-        break;
-    case "":
-        result = num1 / num2;
-        break;
-    default:
-        alert("Неизвестная опкрация!")
-        break;
+function calclulate(){
+    let result = NaN;
+    switch(sign){
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+       case "*":
+           result = num1 * num2;
+           break;
+        case "/":
+             if (0 != num2) result = num1 / num2; 
+             break;
+         default:
+             alert("Неизвестная опкрация!")
+             break;
+    }
+    return result;
 }
+
+function calclulator() {
+    if (!confirm('Запустить калькулятор?')) return false;
+
+    let num1, num2, sign, result = NaN, a = 0;
+
+    num1 = Number(prompt('введите первое число:','0'));
+    num2 = Number(prompt('введите первое число:','0'));
+    sign = prompt('введите действие(+,-,*,/):','+');
+    
+    result = calclulate(num1,num2,sign);
 
 if(isNaN(result)){
     alert("Некоректный ввод!")
 } else {
     alert(`Ответ: ${result}`)
 }
+return true;
+}
+
+let stat = true;
+while (stat) stat = calclulator(); 
